00aaf40151c63b93be8d3dda6acf149bbefe7e0b
Turkina Alena <dev@alena.pw>
2014-06-10 22:49:42 +0400

    flag on move

--- snake/main.cpp
+++ snake/main.cpp
@@ -12,6 +12,10 @@
 #include <GL/gl.h>
 #endif
 
+bool choice = false;
+enum moves {
+    LEFT, RIGHT, UP, DOWN
+};
 const int SCALE = 25;
 
 int N=30,M=20;
@@ -55,24 +59,25 @@ void Tick() //изменение головного элемента
         s[i].y=s[i-1].y;}
 
 //перемещение головы
-    
+    if (choice)
+    {
+//ходит по 90 градусов
     int a=0;
     int b=0;
-//ходит по 90 градусов
-/*  for (int i=0;i<1;i++)
+    for (int i=0;i<num;i++)
     {
-    while (a==0 && b==0){
-        a=m[i].x-s[0].x;
-        b=m[i].y-s[0].y;
-        if(a>0) s[0].x+=1;
-        else if (b>0) s[0].y+=1;
-        else if (a<0) s[0].x-=1;
-        else if (b<0) s[0].y-=1;
-       
+        while (a==0 && b==0)
+        {
+            a=m[i].x-s[0].x;
+            b=m[i].y-s[0].y;
+            if(a>0) { s[0].x+=1;}
+            else if (b>0 ){s[0].y+=1;}
+            else if (a<0) {s[0].x-=1;}
+            else if (b<0) {s[0].y-=1;}
+       }
     }
     }
-*/
- for (int i=0;i<1;i++)
+    /*for (int i=0;i<1;i++)
     {do {
         b=m[i].y-s[0].y;
         if(b>0) s[0].y+=1;
@@ -88,12 +93,13 @@ void Tick() //изменение головного элемента
         else s[0].x-=1;
     }
     }
-
-    //if (dir==0) s[0].y+=1;
-    //if (dir==1) s[0].x-=1;
-    //if (dir==2) s[0].x+=1;
-    //if (dir==3) s[0].y-=1;
-
+*/
+    else{
+    if (dir==UP) s[0].y+=1;
+    if (dir==LEFT) s[0].x-=1;
+    if (dir==RIGHT) s[0].x+=1;
+    if (dir==DOWN) s[0].y-=1;
+    }
 //проверка наложения координат яблока и головы
     for (int i=0;i<1;i++)
         if ((s[0].x==m[i].x) && (s[0].y==m[i].y))
@@ -102,13 +108,12 @@ void Tick() //изменение головного элемента
             m[i].New(); //создание нового яблока
         }
     
-    if (s[0].x>N) dir=1;  if (s[0].x<0) dir=2;
-    if (s[0].y>M) dir=3;  if (s[0].y<0) dir=0;
+    if (s[0].x>N) dir=LEFT;  if (s[0].x<0) dir=RIGHT;
+    if (s[0].y>M) dir=DOWN;  if (s[0].y<0) dir=UP;
     
     for (int i=1;i<num;i++)
-        if (s[0].x==s[i].x && s[0].y==s[i].y)  num=i;
+    if (s[0].x==s[i].x && s[0].y==s[i].y)  num=i;
 }
-
 
 
 void DrawField() //поле
@@ -141,7 +146,7 @@ void display() {
         m[i].DrawApple();
     
     glFlush(); //вывод на экран
-   // glutSwapBuffers();
+    //glutSwapBuffers();
 }
 
 
@@ -152,20 +157,18 @@ void KeyboardEvent(int key, int a, int b) //управление змейкой
         case 27:
 			exit(0); // выход из приложения по нажатию ESC
 			break;
-        case 101 : 	dir=0; break;
-        case 102 :  dir=2; break;
-        case 100 :  dir=1; break;
-        case 103 :  dir=3; break;
+        case 101 : 	dir=UP; break;
+        case 102 :  dir=RIGHT; break;
+        case 100 :  dir=LEFT; break;
+        case 103 :  dir=DOWN; break;
     }
 }
 
 void timer(int = 0)
 {
     display();
-    
     Tick();
-    
-    glutTimerFunc(200,timer,0);
+    glutTimerFunc(100,timer,0);
 }
 
 int main(int argc, char **argv) {
@@ -190,7 +193,7 @@ int main(int argc, char **argv) {
     
     glutDisplayFunc (display);
     glutSpecialFunc(KeyboardEvent);
-    glutTimerFunc(200,timer,0); //обявление таймера
+    glutTimerFunc(10,timer,0); //обявление таймера
     
     glutMainLoop(); //основной цикл программы
 }
